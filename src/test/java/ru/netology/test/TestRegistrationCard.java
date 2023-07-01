f032341321dffab670790d016179d776851ad82f
IraKolbasina <irenLev@yandex.ru>
2023-07-01 17:51:48 +0300

    First Commit

---
+++ src/test/java/ru/netology/test/TestRegistrationCard.java
@@ -0,0 +1,49 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CallbackTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+// убедитесь, что файл chromedriver.exe расположен именно в каталоге C:\tmp
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+    }
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+    @Test
+    void shouldTestSomething() {
+        driver.get("http://localhost:9999");
+    }
+    @Test
+    void shouldSubmitRequest() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Василий");
+        elements.get(1).sendKeys("+79270000000");
+        driver.findElement(By.className("checkbox__control")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("alert-success")).getText();
+        assertEquals("Ваша заявка успешно отправлена!", text.trim());
+    }
+
+}
